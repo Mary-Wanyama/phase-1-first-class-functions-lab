f000ea01463172321202f9b5fbf874753509cf3a
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-08-18 11:54:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+let arr = ['Billy', 'Broddy', 'Jack', 'Nate', 'Dakota'];
+
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0, 2);
+}
+const returnLastTwoDrivers = function(arr) {
+    return arr.slice(arr.length-2);
+}
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiply){
+    
+    return num => multiply * num 
+}
+
+ function fareDoubler(createFareMultiplier) {
+    return createFareMultiplier * 2
+ }
+ function fareTripler(createFareMultiplier) {
+
+    return createFareMultiplier * 3
+ }
+
+const selectDifferentDrivers = function(arr, returnTwoDrivers) {
+    return returnTwoDrivers(arr);
+}
+
+
+
+
+
+
+
+// const fareDoubler = createFareMultiplier(2);
+
+// const fareTripler = createFareMultiplier(3);
+
+// const selectDifferentDrivers = (drivers, returnTwoDrivers) => returnTwoDrivers(drivers)
